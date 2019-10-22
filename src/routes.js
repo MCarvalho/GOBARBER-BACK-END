8c63da33f1a266abd612ec41a1cd274347b47b9f
MCarvalho <178carvalho@gmail.com>
2019-10-22 02:12:05 -0300

    Avatar User, List Provider, Migration/Model Appointment:

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
+import FileController from './app/controllers/FileController';
 import UserController from './app/controllers/UserContoller';
+import ProviderController from './app/controllers/ProviderController';
 import SessionContoller from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 // rotas sem verificacao de sessao
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionContoller.store);
@@ -12,5 +17,7 @@ routes.post('/sessions', SessionContoller.store);
 routes.use(authMiddleware);
 // rotas com confirmacao de sessao
 routes.put('/users', UserController.update);
+routes.get('/providers', ProviderController.index);
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
