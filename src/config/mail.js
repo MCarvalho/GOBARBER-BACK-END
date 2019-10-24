09f2379fc89a9a9ae17a46b766068cfccff6c702
MCarvalho <178carvalho@gmail.com>
2019-10-24 15:25:09 -0300

    Appontments, Mails, Notifications ...

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'dde5c15e0d1087',
+    pass: '58b31c8165b72f',
+  },
+  default: {
+    from: 'Equipe GoBerber <noreply@gobarber.com>',
+  },
+};
--- src/routes.js
+++ src/routes.js
@@ -6,18 +6,33 @@ import FileController from './app/controllers/FileController';
 import UserController from './app/controllers/UserContoller';
 import ProviderController from './app/controllers/ProviderController';
 import SessionContoller from './app/controllers/SessionController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 // rotas sem verificacao de sessao
 routes.post('/users', UserController.store);
+
 routes.post('/sessions', SessionContoller.store);
 
 routes.use(authMiddleware);
 // rotas com confirmacao de sessao
 routes.put('/users', UserController.update);
+
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
+routes.delete('/appointments/:id', AppointmentController.delete);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
